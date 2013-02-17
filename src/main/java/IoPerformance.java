94364445bde14885c234b89a4930b5df04b98a3b
Christian Schlichtherle <christian@schlichtherle.de>
2013-02-17 19:48:29 +0100

    Simplification.

--- src/main/java/IoPerformance.java
+++ src/main/java/IoPerformance.java
@@ -23,21 +23,21 @@ public class IoPerformance {
         final File tmpdir = new File(0 < args.length
                 ? args[0]
                 : getProperty("java.io.tmpdir"));
-        out .append("<!DOCTYPE html>\n")
-            .append("<html>\n")
-            .append("  <head>\n")
-            .append("    <title>I/O Performance Test</title>\n")
-            .append("    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=" + Charset.defaultCharset().name() + "\">\n")
-            .append("  </head>\n")
-            .append("  <body>\n")
-            .append("    <table>\n")
-            .append("      <caption>Results for temp dir <code>" + tmpdir + "<code></caption>\n")
-            .append("      <thead align=\"right\"><tr><th>Iteration</th><th>Sustainable<br>Write Rate<br>[KB/s]</th><th>Sustainable<br>Read Rate<br>[KB/s]</th></tr></thead>\n")
-            .append("      <tbody align=\"right\">\n");
-        for (int i = 1; i <= ITERATIONS; i++) {
-            rnd.nextBytes(data);
-            final File file = File.createTempFile("tmp", null, tmpdir);
-            try {
+        final File file = File.createTempFile("tmp", null, tmpdir);
+        try {
+            out .append("<!DOCTYPE html>\n")
+                .append("<html>\n")
+                .append("  <head>\n")
+                .append("    <title>I/O Performance Test</title>\n")
+                .append("    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=" + Charset.defaultCharset().name() + "\">\n")
+                .append("  </head>\n")
+                .append("  <body>\n")
+                .append("    <table>\n")
+                .append("      <caption>Results for temp dir <code>" + tmpdir + "<code></caption>\n")
+                .append("      <thead align=\"right\"><tr><th>Iteration</th><th>Sustainable<br>Write Rate<br>[KB/s]</th><th>Sustainable<br>Read Rate<br>[KB/s]</th></tr></thead>\n")
+                .append("      <tbody align=\"right\">\n");
+            for (int i = 1; i <= ITERATIONS; i++) {
+                rnd.nextBytes(data);
                 final long write = nanoTime();
                 {
                     final OutputStream out = new FileOutputStream(file);
@@ -63,13 +63,13 @@ public class IoPerformance {
                         i,
                         COEFFICIENT / (read - write),
                         COEFFICIENT / (done - read));
-            } finally {
-                file.delete();
             }
+            out .append("      </tbody>\n")
+                .append("    </table>\n")
+                .append("  </body>\n")
+                .append("</html>\n");
+        } finally {
+            file.delete();
         }
-        out .append("      </tbody>\n")
-            .append("    </table>\n")
-            .append("  </body>\n")
-            .append("</html>\n");
     }
 }
